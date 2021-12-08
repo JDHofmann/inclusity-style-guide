a2ee8923f76e4997f035f03625ee3fa90625de29
jd <justin_d_brewer-hofmann@progressive.com>
2021-12-08 12:10:35 -0500

    fixed menu

--- menu-02-src/main.js
+++ menu-02-src/main.js
@@ -1,17 +1,20 @@
-MenuButton.prototype.open = function () {
-    this.button.setAttribute('aria-expanded', true);
-    this.menu.hidden = false;
-    this.menu.querySelector(':not([disabled])').focus();
 
-    return this;
-}
-this.button.addEventListener('keydown', function (e) {
-    if (e.keyCode === 40) {
-        this.open();
-    }
-}.bind(this));
+document.querySelectorAll("[data-toggle~=dropdown]").forEach(function (e) {
 
-exampleMenuButton = new MenuButton(document.querySelector('[aria-haspopup]'));
-console.log(document.querySelector('[aria-haspopup]'))
+    e.setAttribute("aria-haspopup", "true"), 
+    e.setAttribute("aria-expanded", "false");
 
-exampleMenuButton.open();
+    var t = document.querySelector(".dropdown-menu");
+
+    t.setAttribute("aria-hidden", "true"),
+
+    (e.onclick = function () {
+
+    if ("true" === e.getAttribute("aria-expanded")) return e.setAttribute("aria-expanded", "false"), t.setAttribute("aria-hidden", "true"), void e.parentNode.classList.remove("dropdown-on");
+
+    return e.setAttribute("aria-expanded", "true"),
+     t.setAttribute("aria-hidden", "false"), e.parentNode.classList.add("dropdown-on"), void t.children[0].focus();
+
+    });
+
+});
